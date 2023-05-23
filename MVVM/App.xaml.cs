390ae6b9581938a4f47ba03f8a36b27f53c8920d
opilane <opilane@HP-CZC2349HTZ>
2023-05-23 15:32:13 +0300

    4

--- MVVM/App.xaml.cs
+++ MVVM/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using MVVM.Views;
+﻿using MVVM.Models;
+using MVVM.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,11 +9,25 @@ namespace MVVM
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "friend.db";
+        public static FriendRepository database;
+        public static FriendRepository Database
+        {
+            get
+            {
+                if (database == null) 
+                {
+                    database = new FriendRepository(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
-            MainPage = new NavigationPage(new FriendsListPage());
+            MainPage = new NavigationPage(new DBListPage());
+            //MainPage = new NavigationPage(new FriendsListPage());
         }
 
         protected override void OnStart()
